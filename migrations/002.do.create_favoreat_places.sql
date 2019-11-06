6c51d8c286c32d0a76af4ceeead96589f4bf10d5
Ben Grabowski <gbowski30@gmail.com>
2019-11-05 18:55:17 -0700

    added second migration

---
+++ migrations/002.do.create_favoreat_places.sql
@@ -0,0 +1,11 @@
+CREATE TABLE favoreat_places (
+    id SERIAL PRIMARY KEY,
+    user INTEGER REFERENCES favoreat_users(id) ON DELETE CASCADE NOT NULL,
+    place_name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    hh BOOLEAN NOT NULL,
+    hh_start TIME,
+    hh_end TIME,
+    notes TEXT,
+    items TEXT []
+);
