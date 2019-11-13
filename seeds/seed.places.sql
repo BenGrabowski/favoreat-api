577ebe47f0fac5f2d7d5e9b22456a835ca3d91d5
Ben Grabowski <gbowski30@gmail.com>
2019-11-12 21:03:35 -0700

    added seed for places table

---
+++ seeds/seed.places.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+INSERT INTO favoreat_places (user_id, place_name, type, hh, hh_start, hh_end, notes, items)
+VALUES
+    (1, 'Test Place 1', 'Restaurant', true, '4:00 PM', '5:00 PM', 'test notes 1', ARRAY ['item1']),
+    (2, 'Test Place 2', 'Bar', false, null, null, 'test notes 2', null),
+    (3, 'Test Place 3', 'Brewery', true, '5:00 PM', '7:00 PM', null, null),
+    (2, 'Test Place 4', 'Bar', false, null, null, 'test notes 4', ARRAY ['item1', 'item2', 'item3']);
+
+COMMIT;
+    
