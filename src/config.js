7da9da225236a3499c6367437f1eeae4d6787c61
Ben Grabowski <gbowski30@gmail.com>
2019-12-09 18:39:54 -0700

    fixed issue editing places

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://favoreat@localhost/favoreat',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/favoreat',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
