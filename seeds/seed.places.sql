f5e2c0cdd604212999bf62c0ff04eceee031c318
Ben Grabowski <gbowski30@gmail.com>
2019-12-16 18:34:36 -0700

    changed happy hour to string

--- seeds/seed.places.sql
+++ seeds/seed.places.sql
@@ -6,10 +6,10 @@ VALUES
     -- (1, 'Test Place 2', 'Bar', false, null, null, 'test notes 2', null),
     -- (1, 'Test Place 3', 'Brewery', true, '5:00 PM', '7:00 PM', null, null),
     -- (1, 'Test Place 4', 'Bar', false, null, null, 'test notes 4', ARRAY ['item1', 'item2', 'item3']),
-    (1, 'Linger', 'Restaurant', true, '4:00 PM', '6:30 PM', 'Open seating at upstairs bar', ARRAY ['Fried Cheese & Shisitos', 'Boa Buns', 'Spring Rolls']),
-    (1, 'Bareclona Wine Bar', 'Restaurant', true, '5:00 PM', '7:00 PM', 'Half Off wine bottles on Sunday', ARRAY ['Croquetas', 'Empenadas']),
-    (1, 'El Five', 'Restaurant', false, null, null, 'Best view of Denver', ARRAY ['Pheasant Flatbread']),
-    (1, 'Bigsbys Folley', 'Winery', true, '3:00 PM', '6:00 PM', null, null);
+    (1, 'Linger', 'Restaurant', 'true', '4:00 PM', '6:30 PM', 'Open seating at upstairs bar', ARRAY ['Fried Cheese & Shisitos', 'Boa Buns', 'Spring Rolls']),
+    (1, 'Bareclona Wine Bar', 'Restaurant', 'true', '5:00 PM', '7:00 PM', 'Half Off wine bottles on Sunday', ARRAY ['Croquetas', 'Empenadas']),
+    (1, 'El Five', 'Restaurant', 'false', null, null, 'Best view of Denver', ARRAY ['Pheasant Flatbread']),
+    (1, 'Bigsbys Folley', 'Winery', 'true', '3:00 PM', '6:00 PM', null, null);
 
 COMMIT;
     
